2a625e2464de074f5a4306e98bc4dd03751b24a6
Chris <orthagonal1@gmail.com>
2017-03-30 15:05:08 -0500

    rest of the tests

--- test/test.githubRoute.js
+++ test/test.githubRoute.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 
 // you can use this snippet to print an sha1 strings for any other packages you want to add for testing:
 const getSig = payloadToSend => `sha1=${crypto.createHmac('sha1', '123').update(JSON.stringify(payloadToSend)).digest('hex')}`;
-/*
+
 test('githubRoute: will bounce if signature key not given', (t) => {
   setup({}, (err, server) => {
     server.settings.app.secret = '123';
@@ -71,7 +71,7 @@ test('githubRoute accepts http signals', (t) => {
     });
   });
 });
-*/
+
 test('githubRoute will trigger before/end event hooks', (t) => {
   setup({}, (err, server) => {
     server.settings.app.secret = '123';
@@ -113,7 +113,6 @@ test('githubRoute will trigger before/end event hooks', (t) => {
         console.log = oldLog;
         t.equal(err, null);
         t.equal(res.statusCode, 200);
-        console.log(allScriptResults)
         t.notEqual(allScriptResults[0].indexOf('before'), -1);
         t.equal(allScriptResults[1].indexOf('the get down'), 0);
         t.equal(allScriptResults[3].indexOf('arrested development season 4'), 0);
@@ -123,78 +122,78 @@ test('githubRoute will trigger before/end event hooks', (t) => {
     });
   });
 });
-//
-// test('githubRoute will trigger event-specific hooks', (t) => {
-//   setup({}, (err, server) => {
-//     server.settings.app.secret = '123';
-//     server.settings.app.scripts = path.join(__dirname, 'scripts');
-//     wreck.post('http://localhost:8080', {
-//       headers: {
-//         'x-github-event': 'push',
-//         'x-hub-signature': 'sha1=2807ea9ca996abd3b063a76b3d088ec7b32e7d72'
-//       },
-//       payload: {
-//         action: 'opened',
-//         issue: {
-//           url: 'https://api.github.com/repos/octocat/Hello-World/issues/1347',
-//           number: 1347
-//         },
-//         repository: {
-//           id: 1296269,
-//           name: 'Goodbye-World',
-//           owner: {
-//             login: 'octocat',
-//             id: 1
-//           },
-//         },
-//         sender: {
-//           login: 'octocat',
-//           id: 1,
-//         }
-//       }
-//     }, (err, res, payload) => {
-//       setTimeout(() => {
-//         t.equal(err, null);
-//         t.equal(res.statusCode, 200);
-//         server.stop(t.end);
-//       }, 200);
-//     });
-//   });
-// });
-//
-// test('githubRoute sets REF_TYPE if passed', (t) => {
-//   setup({}, (err, server) => {
-//     server.settings.app.secret = '123';
-//     server.settings.app.scripts = path.join(__dirname, 'scripts');
-//     const payloadToSend = {
-//       ref_type: 'the type',
-//       action: 'opened',
-//       issue: {
-//         url: 'https://api.github.com/repos/octocat/Hello-World/issues/1347',
-//         number: 1347
-//       },
-//       repository: {
-//         id: 1296269,
-//         full_name: 'octocat/Hello-World',
-//         owner: {
-//           login: 'octocat',
-//           id: 1
-//         },
-//       },
-//       sender: {
-//         login: 'octocat',
-//         id: 1,
-//       }
-//     };
-//     wreck.post('http://localhost:8080', {
-//       headers: {
-//         'x-github-event': 'create',
-//         'x-hub-signature': 'sha1=d574eada9805392c009f4623813782eccecd3b39'
-//       },
-//       payload: payloadToSend
-//     }, () => {
-//       t.equal(process.env.REF_TYPE, 'the type', 'ref type will set the ENV.REF_TYPE variable');
-//       server.stop(t.end);
-//     });
-//   });
-// });
+
+test('githubRoute will trigger event-specific hooks', (t) => {
+  setup({}, (err, server) => {
+    server.settings.app.secret = '123';
+    server.settings.app.scripts = path.join(__dirname, 'scripts');
+    wreck.post('http://localhost:8080', {
+      headers: {
+        'x-github-event': 'push',
+        'x-hub-signature': 'sha1=2807ea9ca996abd3b063a76b3d088ec7b32e7d72'
+      },
+      payload: {
+        action: 'opened',
+        issue: {
+          url: 'https://api.github.com/repos/octocat/Hello-World/issues/1347',
+          number: 1347
+        },
+        repository: {
+          id: 1296269,
+          name: 'Goodbye-World',
+          owner: {
+            login: 'octocat',
+            id: 1
+          },
+        },
+        sender: {
+          login: 'octocat',
+          id: 1,
+        }
+      }
+    }, (err, res, payload) => {
+      setTimeout(() => {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        server.stop(t.end);
+      }, 200);
+    });
+  });
+});
+
+test('githubRoute sets REF_TYPE if passed', (t) => {
+  setup({}, (err, server) => {
+    server.settings.app.secret = '123';
+    server.settings.app.scripts = path.join(__dirname, 'scripts');
+    const payloadToSend = {
+      ref_type: 'the type',
+      action: 'opened',
+      issue: {
+        url: 'https://api.github.com/repos/octocat/Hello-World/issues/1347',
+        number: 1347
+      },
+      repository: {
+        id: 1296269,
+        full_name: 'octocat/Hello-World',
+        owner: {
+          login: 'octocat',
+          id: 1
+        },
+      },
+      sender: {
+        login: 'octocat',
+        id: 1,
+      }
+    };
+    wreck.post('http://localhost:8080', {
+      headers: {
+        'x-github-event': 'create',
+        'x-hub-signature': 'sha1=d574eada9805392c009f4623813782eccecd3b39'
+      },
+      payload: payloadToSend
+    }, () => {
+      t.equal(process.env.REF_TYPE, 'the type', 'ref type will set the ENV.REF_TYPE variable');
+      server.stop(t.end);
+    });
+  });
+});
