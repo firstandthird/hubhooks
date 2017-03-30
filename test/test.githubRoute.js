e1a49ad7cfb08b5e3cf87d3aee65ae8dc9bccfaa
Chris <orthagonal1@gmail.com>
2017-03-30 16:20:52 -0500

    add to routes

--- test/test.githubRoute.js
+++ test/test.githubRoute.js
@@ -102,6 +102,7 @@ test('githubRoute will trigger before/end event hooks', (t) => {
       allScriptResults.push(data);
     };
     server.on('tail', () => {
+      console.log(allScriptResults)
       t.notEqual(allScriptResults[0].indexOf('before'), -1);
       t.equal(allScriptResults[1].indexOf('the get down'), 0);
       t.equal(allScriptResults[4].indexOf('arrested development season 4'), 0);
